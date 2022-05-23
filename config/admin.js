4ecc4aec2802b2c2374952c299783d4d2bb86293
Nevo David <nevo@notu.co>
2022-05-23 14:10:42 +0700

    feat(): salt

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'someRandomLongString'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '8009f8775ea03f9b28f9937aefcc103a'),
   },
